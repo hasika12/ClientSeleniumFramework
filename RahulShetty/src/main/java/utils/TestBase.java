c3c7cafe5634deaa2c78bc64ef047e306585d5a4
saii2882 <saii2882@gmail.com>
2020-05-01 17:44:17 +0530

    Commited on 1st May 2020

---
+++ RahulShetty/src/main/java/utils/TestBase.java
@@ -0,0 +1,66 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+public class TestBase {
+	
+	Properties prop = new Properties();
+	public WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	
+	public WebDriver initializeDriver() throws Exception
+	{
+		FileInputStream fis = new FileInputStream("F:\\Maestrano-WorkPlace\\RahulShetty\\src\\main\\java\\utils\\data.properties");
+		
+		prop.load(fis);
+		
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/chromedriver.exe");
+			ChromeOptions co = new ChromeOptions();
+			//co.addArguments("-headless");
+			co.setHeadless(false);
+			driver = new ChromeDriver(co);
+		}
+		
+		else if(prop.getProperty("browser").equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", projectPath+"/drivers/geckodriver.exe");
+			
+			driver = new FirefoxDriver();
+		}
+		
+		else if(prop.getProperty("browser").equalsIgnoreCase("IE"))
+		{
+			System.setProperty("webdriver.ie.driver", projectPath+"/drivers/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+		
+		else
+		{
+			System.out.println("Please specify a valid browser");
+		}
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://ui.cogmento.com");
+		
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		return driver;
+	}
+
+}
